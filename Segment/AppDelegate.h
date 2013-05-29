7ae22679704b21f16927bc523f2e7abd3d8f156b
huangfulei <huangfl@m15.cn>
2013-05-29 14:21:35 +0800

    Initial Commit

---
+++ Segment/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Segment
+//
+//  Created by 黄富磊 on 13-5-29.
+//  Copyright (c) 2013年 huangfulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
