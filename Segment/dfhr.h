c0e6ed5eeb03faf8acff98f843797582b08c4126
huangfulei <huangfl@m15.cn>
2013-06-17 14:51:22 +0800

    first

---
+++ Segment/dfhr.h
@@ -0,0 +1,13 @@
+//
+//  dfhr.h
+//  Segment
+//
+//  Created by 黄富磊 on 13-6-17.
+//  Copyright (c) 2013年 huangfulei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface dfhr : UIView
+
+@end
---
+++ Segment/dfhr.m
@@ -0,0 +1,31 @@
+//
+//  dfhr.m
+//  Segment
+//
+//  Created by 黄富磊 on 13-6-17.
+//  Copyright (c) 2013年 huangfulei. All rights reserved.
+//
+
+#import "dfhr.h"
+
+@implementation dfhr
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
